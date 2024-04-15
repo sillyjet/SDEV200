2275c5482d5f3161fa90a28146be8ba6536fb835
JJ Thomson <144045273+sillyjet@users.noreply.github.com>
2024-04-15 02:47:44 -0400

    Update MyLinkedList.java

--- MyLinkedList/MyLinkedList.java
+++ MyLinkedList/MyLinkedList.java
@@ -1,1 +1,6 @@
-E
+I do not understand what this assignment is asking me to do. I see that it is requiring me to create a new class called TwoWayLinkedList that implements the interface
+called MyList, however this interface doesn't exist within the code in Listing 24.5, and although I found the interface within the test code for assignment 24.1. this
+doesn't change the fact that all the methods I am supposedly supposed to implement from MyLinkedList are empty because they are supposed to be completed in separate
+assignments. I am not sure what I am missing. My family wasn't even able to decipher the instructions.
+
+Side note: I love that E is used as a generic object. It's a good letter.
