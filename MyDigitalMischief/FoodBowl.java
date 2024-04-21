0c7e1a2e9a2f3bd8bdad7e8f19892acfb6a7e7f5
JJ Thomson <144045273+sillyjet@users.noreply.github.com>
2024-04-21 19:33:51 -0400

    Create FoodBowl.java

---
+++ MyDigitalMischief/FoodBowl.java
@@ -0,0 +1,7 @@
+package com.example.mydigitalmischief;
+
+public class FoodBowl extends Material {
+    public int fullness; // number 0 to 10 for how full it is
+    public String colorHex; // hexadecimal string for color
+    public String color; // name of color
+}
